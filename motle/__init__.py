55c373d45bd5b04fb51fac06c2870a377acc5bae
Gauthier Chaty <gauthier.chaty@gmail.com>
2022-01-13 17:21:39 +0100

    Add setup

---
+++ motle/__init__.py
@@ -0,0 +1,2 @@
+from .motle import *
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='motle',
+    version='0.1',
+    description='Motle is a library for finding words in wordle game',
+    url='https://github.com/Gokender/motle',
+    author='Gauthier Chaty',
+    author_email='gauthier.chaty@outlook.fr',
+    license='MIT',
+    packages=['motle']
+)
