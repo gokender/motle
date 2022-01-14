82cf6f2dab1f1ba3c0d56346912e0a3112e641af
Gauthier Chaty <gauthier.chaty@gmail.com>
2022-01-14 11:32:22 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
+from pathlib import Path
+
+import motle
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='motle',
-    version='0.1',
+    version=motle.__version__,
     description='Motle is a library for finding words in wordle game',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/Gokender/motle',
     author='Gauthier Chaty',
-    author_email='gauthier.chaty@outlook.fr',
+    author_email='gauthier.chaty+pypi@outlook.fr',
     license='MIT',
     packages=['motle']
 )
